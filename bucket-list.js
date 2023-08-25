dee1d13c128dfa9187ece033f93bc08ec5876535
Rometikene <142340594+Rometikene@users.noreply.github.com>
2023-08-25 09:46:26 +0300

    Add files via upload

--- bucket-list.js
+++ bucket-list.js
@@ -1,35 +1,89 @@
-document.addEventListener("DOMContentLoaded", function (){
-let addToDoButton = document.getElementById('addToDo');
-let inputField = document.getElementById('inputField');
-let toDoItem = document.getElementById('toDoItem');
+document.addEventListener("DOMContentLoaded", function () {
+    let addToDoButton = document.getElementById('addToDo');
+    let inputField = document.getElementById('inputField');
+    let toDoItem = document.getElementById('toDoItem');
 
-addToDoButton.addEventListener('click', function(){
-    const taskText = inputField.value.trim();
-    if (taskText !== "") {
-        const existingTasks = toDoItem.querySelectorAll("div").length;
+    // Load to-do items from local storage when the page loads
+    loadToDoItems();
 
-        if (existingTasks < 10) {
-        const taskItemHTML = `
-        <div class="itemToDo">
-            <span class="toDoText">${taskText}</span>
-            <button class="delete"><img src="images/trash-solid.png"></button>
-            <button class="check"><img src="images/check-solid.png"></button>
-        </div>
-        `;
+    addToDoButton.addEventListener('click', function () {
+        const taskText = inputField.value.trim();
+        if (taskText !== "") {
+            const existingTasks = toDoItem.querySelectorAll(".itemToDo").length;
 
-        toDoItem.innerHTML += taskItemHTML;
-        inputField.value= "";
+            if (existingTasks < 9) {
+                const taskItemHTML = `
+                    <div class="itemToDo">
+                        <span class="toDoText">${taskText}</span>
+                        <button class="delete"><img src="images/trash-solid.png"></button>
+                        <button class="check"><img src="images/check-solid.png"></button>
+                    </div>
+                `;
 
+                toDoItem.innerHTML += taskItemHTML;
+                inputField.value = "";
+
+                saveToDoItems();
+                attachButtonListeners();
+            } else {
+                alert("You can only have up to 9 items at once.");
+            }
+        }
+    });
+
+    function saveToDoItems() {
+        const taskItems = document.querySelectorAll(".itemToDo");
+        const tasks = [];
+
+        taskItems.forEach(taskItem => {
+            const taskText = taskItem.querySelector(".toDoText").textContent;
+            const isTaskChecked = taskItem.querySelector(".check").classList.contains("checked-button");
+
+            tasks.push({ text: taskText, checked: isTaskChecked });
+        });
+
+        localStorage.setItem("toDoItems", JSON.stringify(tasks));
+    }
+
+    function loadToDoItems() {
+        const savedTasks = JSON.parse(localStorage.getItem("toDoItems"));
+
+        if (savedTasks) {
+            savedTasks.forEach(task => {
+                const taskItemHTML = `
+                    <div class="itemToDo">
+                        <span class="toDoText">${task.text}</span>
+                        <button class="delete"><img src="images/trash-solid.png"></button>
+                        <button class="check"><img src="images/check-solid.png"></button>
+                    </div>
+                `;
+
+                toDoItem.innerHTML += taskItemHTML;
+
+                const lastTaskItem = toDoItem.querySelector(".itemToDo:last-child");
+                if (task.checked) {
+                    lastTaskItem.querySelector(".toDoText").classList.add("checked-text");
+                    lastTaskItem.querySelector(".check").classList.add("checked-button");
+                }
+            });
+
+            attachButtonListeners();
+        }
+    }
+
+    function attachButtonListeners() {
         const deleteButtons = document.querySelectorAll(".delete");
         deleteButtons.forEach(deleteButton => {
-            deleteButton.addEventListener("click", function() {
+            deleteButton.addEventListener("click", function () {
                 this.parentNode.remove();
+                saveToDoItems();
             });
         });
+
         const checkButtons = document.querySelectorAll(".check");
         checkButtons.forEach(checkButton => {
             checkButton.addEventListener("click", function () {
-                const taskTextSpan = this.parentNode.querySelector("span");
+                const taskTextSpan = this.parentNode.querySelector(".toDoText");
 
                 if (!taskTextSpan.classList.contains("checked-text")) {
                     taskTextSpan.classList.add("checked-text");
@@ -38,11 +92,8 @@ addToDoButton.addEventListener('click', function(){
                     taskTextSpan.classList.remove("checked-text");
                     this.classList.remove("checked-button");
                 }
+                saveToDoItems();
             });
         });
-    } else {
-        alert("You can only have up to 10 items at once.")
     }
-    }
-});
 });
